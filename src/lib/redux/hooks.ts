230c22e90932a5ccd2cb4fdc0b0d666f82156a3b
ahmed abbes <ahmedyoussefabbes001@gmail.com>
2025-09-09 06:09:10 +0000

    ## Error Type Console Error
    ## Error Message
    ❌ [AuthAPI] Échec de la ré

--- src/lib/redux/hooks.ts
+++ src/lib/redux/hooks.ts
@@ -1,9 +1,10 @@
 // src/lib/redux/hooks.ts
 import { useDispatch, useSelector } from 'react-redux'
 import type { TypedUseSelectorHook } from 'react-redux'
-import type { RootState, AppDispatch, AppStore } from './store'
+import type { RootState, AppDispatch } from './store'
+import { store } from './store';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
-export const useAppStore = (): AppStore => store; // Corrected to a function that returns the store
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppStore = () => store;
