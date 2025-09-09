158d83eae1af7851f95e382a92897cc561d8ec76
ahmed abbes <ahmedyoussefabbes001@gmail.com>
2025-09-09 03:33:18 +0000

    [{ 	"resource": "/home/user/studio/src/components/forms/AssignmentForm/i

--- src/components/forms/AssignmentForm/index.ts
+++ src/components/forms/AssignmentForm/index.ts
@@ -1,2 +1,3 @@
+// src/components/forms/AssignmentForm/index.ts
 export { default } from "./AssignmentForm";
-export type { AssignmentFormProps } from "./types";
+export type { AssignmentFormProps } from "../types";
