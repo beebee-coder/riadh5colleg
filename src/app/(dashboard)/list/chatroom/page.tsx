7892a1a5e1eb9b0dca61aabbab0167aae13ecf59
ahmed abbes <ahmedyoussefabbes001@gmail.com>
2025-09-09 06:38:28 +0000

    [{ 	"resource": "/home/user/studio/src/app/(dashboard)/list/chatroom/pag

--- src/app/(dashboard)/list/chatroom/page.tsx
+++ src/app/(dashboard)/list/chatroom/page.tsx
@@ -3,8 +3,8 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useAppSelector } from '@/hooks/redux-hooks';
-import { selectCurrentUser, selectIsAuthenticated, selectIsAuthLoading } from '@/lib/redux/features/auth/authSlice';
+import { useAppSelector } from '@/lib/redux/hooks';
+import { selectCurrentUser, selectIsAuthenticated, selectIsAuthLoading } from '@/lib/redux/slices/authSlice';
 import { Spinner } from '@/components/ui/spinner';
 import { Role } from '@/types';
 
